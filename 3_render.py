a39f30e21998e20360407cb4a89d923da9b27406
lintangwisesa <lintangwisesa@ymail.com>
2018-12-26 17:00:18 +0700

    flask basic

---
+++ 3_render.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+    # render html file from templates dir
+
+if __name__ == '__main__':
+    app.run(debug = True)
+    # automatically restart when there is a change
