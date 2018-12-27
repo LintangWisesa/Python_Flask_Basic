37eeca882cd89bd2f8e28122102240a0925dceae
lintangwisesa <lintangwisesa@ymail.com>
2018-12-27 11:00:57 +0700

    python flask

---
+++ 9_requestArgs.py
@@ -0,0 +1,48 @@
+from flask import abort, Flask, jsonify, request, render_template
+
+app = Flask(__name__)
+
+Students = [
+    {
+        'id': 1,
+        'name': 'Andi',
+        'age': 21
+    },
+    {
+        'id': 2,
+        'name': 'Budi',
+        'age': 22
+    },
+    {
+        'id': 1,
+        'name': 'Caca',
+        'age': 23
+    }
+]
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/data', methods=['GET'])
+def data():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return jsonify({'error': 'No id field provided on URL'})
+    results = []
+    for student in Students:
+        if student['id'] == id:
+            results.append(student)
+            return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+# try to GET:
+# localhost:5000/data?id=1
+# localhost:5000/data?id=2
